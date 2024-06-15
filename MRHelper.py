16f80706780bd7fba5f90ff957d9ad2ca8a1275a
UselessFodder <uselessfodder@gmail.com>
2024-06-14 23:01:41 -0600

    Category Window + Table Buttons (Initial)
    - When category button is pressed, a new window is dynamically opened that has buttons for all tables in that category and a display label for the results
    - When table buttons are pressed, the results are added as a new line to the display label
    - A CLEAR button was added that removes all text from the display label
    - Added "clear_label" function to enable the above button functionality
    - Added "Name" key to JSON files
    - Many hard-coded values changed to dynamic ones
    - Fixed incorrect Abilities JSON

--- MRHelper.py
+++ MRHelper.py
@@ -10,6 +10,7 @@ import tkinter as tk
 import random
 import json
 import os
+from functools import partial
 
 # ***TODO: Move tables to external library
 # Define the monster features table
@@ -42,31 +43,61 @@ table_types = []
 # load all tables within the tables directory
 tables = load_tables('tables')
 
+def sub_menu(category):
+    #open a new window with category name
+    window = tk.Tk()
+    #window.geometry("400x200")
+    window.title(f"{category} Tables")
+    sub_panel = tk.Frame(window, width=200, height=200)
+    sub_panel.grid(row=0, column=0, padx=10)
+    sub_panel.pack_propagate(0)
+    sub_label = tk.Label(sub_panel, text="")
+    sub_label.pack()
+    #sub_panel.pack(pady=20)
+    sub_buttons = tk.Frame(window, padx=10)
+    sub_buttons.grid(row=0, column=1)
+    
+    #create buttons for all tables in category
+    for x in tables:
+        current_table = tables.get(x)
+        if current_table.get("Category") == category:
+            new_button = tk.Button(sub_buttons, text=x, command=partial(generate,x,sub_label))
+            new_button.pack(pady=10)
+            
+    #add clear button to erase label
+    clear_button = tk.Button(sub_buttons, text='Clear', font='Helvetica 12 bold', command=partial(clear_label,sub_label))
+    clear_button.pack(pady=10)
+    
+    window.mainloop()
+
+def clear_label(label):
+    label.config(text='')
+
 def roll_dice():
     return random.randint(1, 6), random.randint(1, 6)
 
-def generate_feature():
-    table_name = 'monster_features'
+def generate(table_name, panel_name):
+    #table_name = 'monster_features'
     group, item = roll_dice()
     feature = tables[table_name][str(group)][item - 1]
     #result_label.config(text=f"Group {group}, Item {item}: {feature}")
-    result_label.config(text=f"Result: ({group}, {item}): {feature}")
+    panel_name.config(text=panel_name["text"] + f"{tables[table_name].get('Name')}: {feature} ({group}, {item})\n")
 
 # Setup the main application window
 root = tk.Tk()
-root.geometry("600x400")
+#root.geometry("600x400")
 root.title("Maze Rats Helper")
 
 # create a frame to hold buttons
 buttons = tk.Frame(root, padx=10)
 
 # Create a button to generate a monster feature
-generate_button = tk.Button(buttons, text="Generate Monster Feature", command=generate_feature)
-generate_button.pack(pady=20)
+#generate_button = tk.Button(buttons, text="Generate Monster Feature", command=partial(generate,'monster_features'))
+#generate_button.pack(pady=20)
 
 # create buttons for all categories
 for x in table_types:
-    new_button = tk.Button(buttons, text=x)
+    new_button = tk.Button(buttons, text=x, command=partial(sub_menu,x))
     new_button.pack(pady=10)
 
 # Label to display the result
